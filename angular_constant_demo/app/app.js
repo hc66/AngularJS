877adbbd77666e329ab66dd068131359f133aa94
Hong Cai <rhett.cai@gmail.com>
2016-05-02 21:16:07 -0400

    main source file

---
+++ angular_constant_demo/app/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hc66 on 4/24/2016.
+ */
+var app = angular.module('Section',[]);
+
+/**
+ * Constant doesn't have dependent injection.
+ */
+ 
+app.constant('all_sections', [1]);
+app.constant('before_sections', [1,2]);
+app.constant('hierarchy_sections', [5,6]);
